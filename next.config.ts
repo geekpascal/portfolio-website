20a35e7b83cf17f4314f5acdf341449c1502847f
Victor Pascal Dike <vipascal99@gmail.com>
2025-02-21 21:57:30 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// export default nextConfig;
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // Other Next.js config options here...
 };
 
 export default nextConfig;
